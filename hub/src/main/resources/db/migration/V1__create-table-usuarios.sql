4618893bbf5f663d65aaa369d852653dcfdfc3a4
Orli Dun <122529721+Orliluq@users.noreply.github.com>
2024-06-19 20:35:21 -0400

    Delete hub/src/main/resources/db/migration directory

--- hub/src/main/resources/db/migration/V1__create-table-usuarios.sql
+++
@@ -1,8 +0,0 @@
-create table usuarios (
-    id bigint not null auto_increment,
-    username varchar(100)not null unique,
-    password varchar(300)not null,
-    name varchar(100)not null,
-    email varchar(100)not null unique,
-    primary key(id)
-);
--- hub/src/main/resources/db/migration/V3__create-table-respuestas.sql
+++
@@ -1,8 +0,0 @@
-create table respuestas (
-    id bigint not null auto_increment,
-    solution varchar(1000)not null,
-    author int not null,
-    topico int not null,
-    date datetime not null,
-    primary key(id)
-);
